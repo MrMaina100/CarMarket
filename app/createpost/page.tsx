a55cd3bcb8c32a13b9ebc59df5fe8c4f4f501df7
MrMaina100 <mburumaina648@gmail.com>
2023-11-21 21:08:23 +0300

    new routes added- createpost, explore, profile

---
+++ app/createpost/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { useState } from "react"
+
+export default function CreatepPostpage() {
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ app/explore/page.tsx
@@ -0,0 +1,11 @@
+export default function Explorepage() {
+  return (
+    <div>
+      <h1>View a variety of cars</h1>
+      <div className="flex flex-col space-4 items-center md:flex-row md:space-x-8 md:space-y-0">
+
+         {/*the cards will go here  */}
+      </div>
+    </div>
+  )
+}
