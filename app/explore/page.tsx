983bfad619fbc75ca39cd25ad054fd32ad5761c3
MrMaina100 <mburumaina648@gmail.com>
2023-12-02 15:41:22 +0300

    minor updates

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -33,7 +33,7 @@ export default async function Explorepage() {
          {/*the cards will go here  */}
         {data ? (
          data.map((apiData)=>(
-          <CarCards  data = {apiData}/>     
+          <CarCards key={apiData.id}  data = {apiData}/>     
                
         
          ))
