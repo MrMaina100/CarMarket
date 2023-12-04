e810fc5040ba58a8d41e2bcf0ddb1c07716abe46
MrMaina100 <mburumaina648@gmail.com>
2023-12-04 12:56:58 +0300

    protected the create a post route

--- app/createpost/page.tsx
+++ app/createpost/page.tsx
@@ -1,6 +1,16 @@
-import Form from "@/components/pages/Form"
+import Form from "@/app/createpost/Form"
+import FormB from "./FormB"
+import FormC from "./FormC"
+import FormD from "./FormD"
+import supabaseServer from "@/lib/utilities/supabaseServer"
+import { redirect } from "next/navigation"
 
-export default function CreatepPostpage() {
+export default async function CreatepPostpage() {
+
+  const {data: {session}} = await supabaseServer.auth.getSession()
+  if(!session){
+    redirect('/signin')
+  }
   return (
     <div>
       <h1>Create your own listing, let people bid on your car</h1>
