dd9d64a54a1690edecf18cc67412e62695508d56
MrMaina100 <mburumaina648@gmail.com>
2023-12-06 00:44:07 +0300

    added github OAuth button

---
+++ components/ui/GithubButton.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { useState } from "react"
+import supabaseClient from "@/lib/utilities/supabaseClient"
+import { Icons } from "./icons"
+import { Button } from "./button"
+import { toast } from "sonner"
+
+
+export default function GithubButton() {
+   const [isloading, setIsLoading] = useState<boolean>(false)
+
+   const handleOAuth = async ()=>{
+      try {
+         setIsLoading(true)
+         
+         await supabaseClient.auth.signInWithOAuth({
+            provider:'github',
+            options:{
+               redirectTo: 'http://localhost:3000/api/auth/confirm'
+            }
+         })
+         
+      } catch (error) {
+         toast.error('something went wrong')
+
+         
+      }finally{
+         setIsLoading(false)
+      }
+   }
+  return (
+    <div>
+      <Button variant='outline' type="button" disabled={isloading} onClick={handleOAuth} className="w-full">
+         {
+            isloading ?(
+               <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            ):(
+               <Icons.gitHub className="mr-2 h-4 w-4"/>
+            )
+         }
+         Github
+
+      </Button>
+
+    </div>
+  )
+}
