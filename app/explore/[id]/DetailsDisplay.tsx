b2e91eb3f7989e6e64540c9f3e5b5285c4f7a8c7
MrMaina100 <mburumaina648@gmail.com>
2023-12-17 22:53:44 +0300

    updates on the display page

---
+++ app/explore/[id]/DetailsDisplay.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import { Separator } from '@/components/ui/separator'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { PropTypes } from '@/lib/types'
+import { Session } from '@supabase/supabase-js'
+import {Swiper, SwiperSlide} from 'swiper/react'
+
+//swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+//required modules
+import { Navigation} from 'swiper/modules'
+
+
+export default function DetailsDisplay({data , session}:{
+  data:PropTypes,
+  session: Session | null
+}) {
+  
+
+ 
+   const userId = session?.user.id
+   
+  return (
+    <>
+    <div className='flex flex-col space-y-4 items-center md:flex-row  md:space-y-0'>
+      {/* images will go here */}
+      <div className='h-80 w-96 md:w-1/2'>
+        <Swiper navigation={true} modules={[Navigation]}>            
+            {
+               data.image_url?.map((images:string)=>(
+                  <SwiperSlide>
+                    <div className='w-96 h-80'>
+                      
+                      <Image src={`https://uqvgrgiggdjwexfirrqu.supabase.co/storage/v1/object/public/car_images/${images}`} alt='multiple car images' width={200} height={300} priority className='w-full h-full object-cover' />  
+
+                    </div>                 
+                     
+
+                  </SwiperSlide>
+               ))
+            }       
+
+        </Swiper>  
+
+
+      </div>
+
+      
+      {/* car details */}
+
+      <div className='mt-4 w-[400px] p-8 justify-center'>
+        <div className='flex justify-between'>
+          <p>Year of manufacture</p>
+          <p>{data.year}</p>
+        </div>
+        <Separator/>
+        <div className='flex justify-between'>
+          <p>Current Location</p>
+          <p>{data.location}</p>
+        </div>
+        <Separator/>       
+        <div className='flex justify-between'>
+          <p>Drive</p>
+          <p>{data.drive}</p>
+        </div>
+        <Separator/>
+        <div className='flex justify-between'>
+          <p>Mileage</p>
+          <p>{data.mileage}</p>
+        </div>
+        <Separator/>
+        <div className='flex justify-between'>
+          <p>Engine Size</p>
+          <p>{data.engine_size}</p>
+        </div>
+        <Separator/>
+        <div className='flex justify-between'>
+          <p>Fuel Type</p>
+          <p>{data.fuel_type}</p>
+        </div>
+        <Separator/>
+        <div className='flex justify-between'>
+          <p>Transmission</p>
+          <p>{data.transmission}</p>
+        </div>
+        <Separator/>
+        <div className='flex justify-between'>
+          <p>Year of manufacture</p>
+          <p>{data.year}</p>
+        </div>       
+
+        {
+          data.id !== userId && (
+            <div>
+              <Button asChild>
+                <Link href={`mailto:${data.profiles?.email}`}>contact owner</Link>
+              </Button>
+
+            </div>
+          )
+        }   
+
+      </div>
+
+    </div>     
+    </>
+  )
+}
+
+
+//  {
+//                data.image_url?.map((images:string)=>(
+//                   <div key={data.id} className='flex-0 flex-shrink-0 flex-basis-full min-w-0'>
+//                      <Image src={`https://uqvgrgiggdjwexfirrqu.supabase.co/storage/v1/object/public/car_images/${images}`} alt='multiple car images' width={700} height={100}/>
+
+//                   </div>
+//                ))
+//             }
--- app/explore/[id]/page.tsx
+++ app/explore/[id]/page.tsx
@@ -1,83 +1,18 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-import { Separator } from '@/components/ui/separator'
+import { createClient } from "@/lib/utilities/supabaseServer"
+import { cookies } from "next/headers"
+import DetailsDisplay from "./DetailsDisplay"
 
-
-export default async function CarListing({params : {id}}: {params:{id:string}}) {
+export default async function CarListing({params : {id}}: {params:{id:string}}) {  
 
   const cookieStore = cookies()
-    const supabase = createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-      },
-    }
-  )
-
-  const {data} = await supabase.from('cars').select().match({id}).single() 
-
+  const supabase = createClient(cookieStore)  
+  const {data } = await supabase.from('cars').select('*, profiles(*)').match({id}).single() 
+  const {data:{session}} =  await supabase.auth.getSession()
+  
   if(!data){
     return <p>something went wrong, it not you its us</p>
   }
-  return (
-    <>
-    <div className='flex flex-col space-y-4 items-center md:flex-row  md:space-y-0'>
-      {/* images will go here */}
+ 
+  return <DetailsDisplay data={data} session={session}/>
 
-      {/* car details */}
-      <p>{data.price}</p>
-
-      <div className='mt-4 w-[300px]'>
-        <div className='flex justify-between'>
-          <p>Year of manufacture</p>
-          <p>{data.year}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Current Location</p>
-          <p>{data.location}</p>
-        </div>
-        <Separator/>       
-        <div className='flex justify-between'>
-          <p>Drive</p>
-          <p>{data.drive}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Mileage</p>
-          <p>{data.mileage}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Engine Size</p>
-          <p>{data.engine_size}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Fuel Type</p>
-          <p>{data.fuel_type}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Transmission</p>
-          <p>{data.transmission}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Year of manufacture</p>
-          <p>{data.year}</p>
-        </div>
-
-
-      </div>
-
-    </div>
-
-      
-    </>
-  )
 }
