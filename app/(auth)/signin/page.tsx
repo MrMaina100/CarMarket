8831d95c9fa685b06931d53cbd58de3783589bd4
MrMaina100 <mburumaina648@gmail.com>
2023-12-18 01:26:53 +0300

    updates on the sign in page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -6,14 +6,14 @@ export default function SignInPage() {
 
   return (
     <>
-    <div className="container h-screen w-screen flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center p-8">
       <Link href='/' className="absolute left-4 top-4 md:left-8 md:top-8">
          <Button>
             Back
          </Button>
       </Link>
 
-      <div className="mx-auto space-y-4 justify-center sm:w-[360px]">
+      <div className=" space-y-4 justify-center w-96 p-8 md:shadow-2xl">
       <h1 className="text-xl font-semibold tracking-tight">
                 Welcome back
               </h1>
