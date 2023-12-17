3df65c7f33d071ff10ef86a7212e263dd81124e3
MrMaina100 <mburumaina648@gmail.com>
2023-12-18 01:32:25 +0300

    update display for card skeletons

--- components/ui/skeletons/CardSkeleton.tsx
+++ components/ui/skeletons/CardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SkeletonCards(){
    return(
       <>
-      <div>
+      <div className="mt-2 flex flex-col space-4 items-center md:flex-row md:space-x-8 md:space-y-0">
          <Skeleton className="w-72">
             <div className="grid gap-2 p-2 h-34">
                {/* img */}
