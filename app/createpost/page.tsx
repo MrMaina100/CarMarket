8959fc94caf924a74a38407324cea31f5ba096e2
MrMaina100 <mburumaina648@gmail.com>
2023-12-17 21:03:44 +0300

    updates

--- app/createpost/page.tsx
+++ app/createpost/page.tsx
@@ -1,20 +1,22 @@
 import Form from "@/app/createpost/Form"
-import FormB from "./FormB"
-import FormC from "./FormC"
-import FormD from "./FormD"
-import supabaseServer from "@/lib/utilities/supabaseServer"
+import { createClient } from "@/lib/utilities/supabaseServer"
+import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
-export default async function CreatepPostpage() {
 
-  const {data: {session}} = await supabaseServer.auth.getSession()
+export default async function CreatepPostpage() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const {data: {session}} = await supabase.auth.getSession()
   if(!session){
     redirect('/signin')
+    
   }
   return (
     <div>
       <h1>Create your own listing, let people bid on your car</h1>
-      <Form/>    
+      <Form session={session}/>            
 
     </div>
   )
