5609aea70cbb669f4731b61e42e7b52d59abd292
MrMaina100 <mburumaina648@gmail.com>
2023-12-17 15:57:53 +0300

    updates on the supabase types and supabase client and server components

--- lib/utilities/supabaseClient.tsx
+++ lib/utilities/supabaseClient.tsx
@@ -1,6 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export default createBrowserClient(
+export default createBrowserClient<Database>(
 process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
