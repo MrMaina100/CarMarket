66d34ad9de246df7542c2ac8708772774891f635
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-12-19 12:48:30 +0300

    Style: Updated DetailsDisplay

--- app/explore/[id]/DetailsDisplay.tsx
+++ app/explore/[id]/DetailsDisplay.tsx
@@ -1,122 +1,111 @@
-'use client'
+"use client";
 
-import { Separator } from '@/components/ui/separator'
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import { PropTypes } from '@/lib/types'
-import { Session } from '@supabase/supabase-js'
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PropTypes } from "@/lib/types";
+import { Session } from "@supabase/supabase-js";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 //swiper styles
-import 'swiper/css'
-import 'swiper/css/navigation'
+import "swiper/css";
+import "swiper/css/navigation";
 
 //required modules
-import { Navigation} from 'swiper/modules'
+import { Navigation } from "swiper/modules";
 
-
-export default function DetailsDisplay({data , session}:{
-  data:PropTypes,
-  session: Session | null
+export default function DetailsDisplay({
+  data,
+  session,
+}: {
+  data: PropTypes;
+  session: Session | null;
 }) {
-  
+  const userId = session?.user.id;
 
- 
-   const userId = session?.user.id
-   
   return (
     <>
-    <div className='flex flex-col space-y-4 p-4 items-center md:flex-row  md:space-y-0'>
-      {/* images will go here */}
-      <div className='h-80 w-96 md:w-1/2'>
-        <Swiper navigation={true} modules={[Navigation]}>            
-            {
-               data.image_url?.map((images:string)=>(
-                  <SwiperSlide key={data.id}>
-                    <div className='w-96 h-80'>
-                      
-                      <Image src={`https://uqvgrgiggdjwexfirrqu.supabase.co/storage/v1/object/public/car_images/${images}`} alt='multiple car images' width={200} height={300} priority className='w-full h-full object-cover' />  
+      <div className="flex flex-col space-y-4 p-4 items-center md:flex-row  md:space-y-0">
+        {/* images will go here */}
+        <div className="h-80 w-96 md:w-1/2  bg-gray-200">
+          <Swiper navigation={true} modules={[Navigation]}>
+            {data.image_url?.map((images: string) => (
+              <SwiperSlide key={data.id}>
+                <div className="w-full h-80 ">
+                  <Image
+                    src={`https://uqvgrgiggdjwexfirrqu.supabase.co/storage/v1/object/public/car_images/${images}`}
+                    alt="multiple car images"
+                    width={400}
+                    height={600}
+                    priority
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
 
-                    </div>                 
-                     
+        {/* car details */}
 
-                  </SwiperSlide>
-               ))
-            }       
+        <div className="mt-4 w-[400px] p-8 justify-center">
+          <div className="flex justify-between">
+            <p>Year of manufacture</p>
+            <p>{data.year}</p>
+          </div>
+          <Separator />
+          <div className="flex justify-between">
+            <p>Current Location</p>
+            <p>{data.location}</p>
+          </div>
+          <Separator />
+          <div className="flex justify-between">
+            <p>Drive</p>
+            <p>{data.drive}</p>
+          </div>
+          <Separator />
+          <div className="flex justify-between">
+            <p>Mileage</p>
+            <p>{data.mileage}</p>
+          </div>
+          <Separator />
+          <div className="flex justify-between">
+            <p>Engine Size</p>
+            <p>{data.engine_size}</p>
+          </div>
+          <Separator />
+          <div className="flex justify-between">
+            <p>Fuel Type</p>
+            <p>{data.fuel_type}</p>
+          </div>
+          <Separator />
+          <div className="flex justify-between">
+            <p>Transmission</p>
+            <p>{data.transmission}</p>
+          </div>
+          <Separator />
+          <div className="flex justify-between">
+            <p>Year of manufacture</p>
+            <p>{data.year}</p>
+          </div>
 
-        </Swiper>  
-
-
+          <div className="mt-2">
+            {data.id !== userId && (
+              <div>
+                <Button asChild>
+                  <Link href={`mailto:${data.profiles?.email}`}>
+                    contact owner
+                  </Link>
+                </Button>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-
-      
-      {/* car details */}
-
-      <div className='mt-4 w-[400px] p-8 justify-center'>
-        <div className='flex justify-between'>
-          <p>Year of manufacture</p>
-          <p>{data.year}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Current Location</p>
-          <p>{data.location}</p>
-        </div>
-        <Separator/>       
-        <div className='flex justify-between'>
-          <p>Drive</p>
-          <p>{data.drive}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Mileage</p>
-          <p>{data.mileage}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Engine Size</p>
-          <p>{data.engine_size}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Fuel Type</p>
-          <p>{data.fuel_type}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Transmission</p>
-          <p>{data.transmission}</p>
-        </div>
-        <Separator/>
-        <div className='flex justify-between'>
-          <p>Year of manufacture</p>
-          <p>{data.year}</p>
-        </div>  
-
-        <div className='mt-2'>
-          {
-          data.id !== userId && (
-            <div>
-              <Button asChild>
-                <Link href={`mailto:${data.profiles?.email}`}>contact owner</Link>
-              </Button>
-
-            </div>
-          )
-        }   
-        
-        </div>     
-
-        
-
-      </div>
-
-    </div>     
     </>
-  )
+  );
 }
-
 
 //  {
 //                data.image_url?.map((images:string)=>(
