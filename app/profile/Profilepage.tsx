164fadd8d888583e7ea575b7fd2383dc164f05a9
MrMaina100 <mburumaina648@gmail.com>
2024-01-22 14:04:32 +0300

    updates on the profile page to only display the car of the user

--- app/profile/Profilepage.tsx
+++ app/profile/Profilepage.tsx
@@ -4,7 +4,18 @@ import Link from 'next/link'
 import CarCard from '@/components/ui/CarCard'
 import { User } from '@supabase/supabase-js'
 import { PropTypes } from "@/lib/types";
+import CarCards from '@/components/ui/CarCard';
+import {
+  Card,
+  CardContent,
+  
+} from "@/components/ui/card"
+import { Badge } from '@/components/ui/badge'
+import { Separator } from '@/components/ui/separator'
+import Image from "next/image"
+
 type Cars = Database['public']['Tables']['cars']['Row']
+
 
 
 export default function ProfileComponent({user, cars}:{
@@ -36,7 +47,25 @@ export default function ProfileComponent({user, cars}:{
         
         {cars?.map((apiData:PropTypes)=>(
         <div key={apiData.id}>
-        <CarCard/>
+        <Card key={apiData.id} className='w-72 overflow-hidden'>
+              <Link href={`/explore/${apiData.id}`}>
+                <CardContent  className="grid gap-2 p-2 h-34">                 
+                  <div className="h-48">
+                     <Image src={`https://uqvgrgiggdjwexfirrqu.supabase.co/storage/v1/object/public/car_images/${apiData.image_url![0]}`} alt="car photo" width={200} height={100} priority className="w-full h-full object-cover " />
+                  </div>                
+                  <Badge className='w-16' variant='outline'>
+                     {apiData.year}
+                  </Badge>                  
+                     {apiData.car_name}                  
+                  <Badge className='w-24'>
+                     {apiData.transmission}
+                  </Badge>
+                  <Separator/>               
+                     {apiData.price}                 
+                  
+               </CardContent>
+              </Link>               
+            </Card>
         </div>
       ))}
         </>
