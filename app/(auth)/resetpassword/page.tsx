c84754aa4932fcacdc053df2eeb372c1452c9b06
MrMaina100 <mburumaina648@gmail.com>
2024-04-26 16:35:26 +0300

    feat: new custom auth flow and ui

--- app/(auth)/resetpassword/page.tsx
+++ app/(auth)/resetpassword/page.tsx
@@ -1,29 +1,48 @@
-import ResetPasswordForm from "./ResetPasswordForm"
-import { createClient } from "@/lib/utilities/supabaseServer"
-import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+  CustomAuthCard,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/custom-auth-card';
+import { updatePassword } from '@/app/actions/auth';
 
-export default async function resetpassword() { 
 
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-  const {data: {session}} = await supabase.auth.getSession()
-   if (!session) {
-    redirect('/sign-in');
-  }
+
+
+export default function page() {
   return (
-    <>
-    <div className="flex flex-col items-center justify-center p-8">   
-      <div className=" space-y-4 justify-center w-96 p-8 md:shadow-2xl">
-      <h1 className="text-xl font-semibold tracking-tight">
-                Reset Password
-      </h1>
-      <ResetPasswordForm/>        
+    <div className="flex justify-center">
+      <CustomAuthCard className="w-[380px]">
+        <CardHeader>
+          <CardTitle>Turdo Trader</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-2">
+          <div className="flex space-y-1 flex-col items-center text-center">
+            <CardTitle>Update Your Password</CardTitle>
+            <CardDescription>
+              Your new Password moving forward
+            </CardDescription>
+          </div>
 
-      </div>
+          <form
+            className="flex flex-col space-y-2 items-center"
+            action={updatePassword}
+          >
+           
+            <label htmlFor="password"></label>
+            <Input type="password" id="password" name="password" placeholder="newpassword" />          
+            <Button type="submit" className="w-full">
+              Continue
+            </Button>
+          </form>
+          
 
+         
+        </CardContent>
+      </CustomAuthCard>
     </div>
-
-    </>
   )
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,127 +1,77 @@
-import Image from "next/image"
-import Link from "next/link"
-import AuthForm from "./AuthForm"
-import authsvg from '../../../public/work-from-home.svg'
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
-export default function SignInpage() {
+import Link from 'next/link';
+import GithubButton from '@/components/ui/GithubButton';
 
+import {
+  CustomAuthCard,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/custom-auth-card';
+
+//server action
+import { handleSignUp } from '@/app/actions/auth';
+
+export default function page() {
   return (
-    <>
-   
-      <div className="container relative items-center h-[800px] flex-col   md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        
-        <div className="relative hidden h-full flex-col bg-gray-100  p-10 text-white dark:border-r lg:flex">
-           <div className="hidden md:block">
-          <Image
-          src={authsvg}
-          width={1200}
-          height={800}
-          alt="Authentication"
-          className="block dark:hidden"
-        />      
-       
-         </div>     
-
-        </div>
-        <div className="lg:p-8">
-          
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center mt-10">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your details below to create your account
-              </p>
-            </div>
-            <AuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href=""
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href=""
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
+    <div className="flex justify-center">
+      <CustomAuthCard className="w-[380px]">
+        <CardHeader></CardHeader>
+        <CardContent className="grid gap-2">
+          <div className="flex space-y-1 flex-col items-center text-center">
+            <CardTitle>Turbo Trader</CardTitle>
+            <CardTitle>Create your account</CardTitle>
+            <CardDescription>
+              You can sign up with email or use Github to create your account
+            </CardDescription>
           </div>
-        </div>
-      </div>
-    
-    </>
-  )
+
+          <form
+            className="flex flex-col space-y-2 items-center"
+            action={handleSignUp}
+          >
+            <label htmlFor="user_name"></label>
+            <Input
+              type="text"
+              id="user_name"
+              name="user_name"
+              placeholder="username"
+            />
+            <label htmlFor="email"></label>
+            <Input type="email" id="email" name="email" placeholder="email" />
+            <label htmlFor="password"></label>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+            />
+            <Button type="submit" className="w-full">
+              Continue
+            </Button>
+          </form>
+          <div className="flex flex-col text-center  space-y-2">
+            <CardDescription>
+              Already have an account? <Link href="/signin">Log in</Link>
+            </CardDescription>
+            <div className="relative mt-2">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs  uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <GithubButton />
+        </CardContent>
+      </CustomAuthCard>
+    </div>
+  );
 }
-
-
-
-
-
-
-
-
-
-// <Card className="w-[500px]">
-//          <CardHeader>
-//             <CardTitle> Create an account Today </CardTitle>
-//          </CardHeader>
-//          {/* our social buttons will be here */}
-
-//          <CardContent>
-           
-//             <form onSubmit={handleSubmit}>
-//              <div className="grid w-full items-center gap-2">
-//                <Label htmlFor="userName">Name</Label>
-//                <Input
-//                id="userName"
-//                value={userName}
-//                onChange={(e)=>setName(e.target.value)}
-               
-//                />
-//                <Label htmlFor="email">Email</Label>
-//                <Input
-//                id="email"
-//                value={email}
-//                onChange={(e)=>setEmail(e.target.value)}
-               
-//                />
-//                <Label htmlFor="password">Password</Label>
-//                <Input
-//                type="password"
-//                id="password"
-//                value={password}
-//                onChange={(e)=>setPassword(e.target.value)}
-               
-//                />
-
-//              </div>
-
-//             <div className="flex justify-between mt-5 items-center">
-//             <Link href='/signin'>
-//             <p className="text-xs ">Already have an Account? Login</p>
-//             </Link>
-
-//             <Button type="submit">
-//                Sign up
-//             </Button>
-
-//          </div>
-
-              
-
-
-//             </form>
-//          </CardContent>
-
-        
-
-
-
-//       </Card>
