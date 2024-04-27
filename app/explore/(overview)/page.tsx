e38323ce23a53e8b454c36ae1b84a58af0b60a01
MrMaina100 <mburumaina648@gmail.com>
2024-04-27 14:10:24 +0300

    feat: update on the explore and display on the car cards

--- app/explore/(overview)/page.tsx
+++ app/explore/(overview)/page.tsx
@@ -1,22 +1,15 @@
-import { Suspense } from "react"
-import CarCards from "@/components/ui/CarCard"
-import { SkeletonCards } from "@/components/ui/skeletons/CardSkeleton"
+import { Suspense } from 'react';
+import CarCards from '@/components/ui/CarCard';
+import { SkeletonCards } from '@/components/ui/skeletons/CardSkeleton';
 
-
-
-export default  function Explorepage() {  
+export default function Explorepage() {
   return (
     <div>
-      <h1>View a variety of cars</h1>
       <div className="mt-2  flex flex-col space-y-4 items-center md:flex-row md:space-x-4 md:space-y-0 md:px-4">
-
-        <Suspense fallback={<SkeletonCards/>}>
-          <CarCards/>
-        </Suspense>        
-        
+        <Suspense fallback={<SkeletonCards />}>
+          <CarCards />
+        </Suspense>
       </div>
-
-      
     </div>
-  )
+  );
 }
